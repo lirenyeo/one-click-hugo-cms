7aab88aef4c41c25fa0704fccc709e44c9331b0b
Liren <lirenyeo@users.noreply.github.com>
2018-04-25 13:36:47 +0800

    Update home.js

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -15,7 +15,7 @@ export default class PostPreview extends React.Component {
 
     return <div>
         <Jumbotron image={image} title={entry.getIn(["data", "title"])} subtitle={entry.getIn(["data", "subtitle"])}/>
-
+        <h1>WAHAHAHAHAHA</h1>
         <div className="bg-grey-1 pv4">
           <div className="flex-l mhn1-l ph3 center mw7">
             <h2 className="f2 b lh-title mb2 w-40-l">{entry.getIn(["data", "blurb", "heading"])}</h2>
